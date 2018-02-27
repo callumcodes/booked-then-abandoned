6d38284101664b7805803f6875ebd8e42642a52d
damien.cooke <damien.cooke@digital.hmrc.gov.uk>
2018-02-27 17:30:32 +0000

    added script to add stuff to the database

---
+++ quickAddRoomBook.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+array=(pink red teal green orange)
+
+for color in "${array[@]}" 
+do
+  printf "$color \n"
+  curl -d '{"lastUsed": 1519746450}' -H "Content-Type: application/json" -X PUT https://booked-then-abandoned.herokuapp.com/room/$color
+  printf "\n" 	
+done
+
+printf "\nFinished!!\n"
